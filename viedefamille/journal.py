2eed36473a20ed4e05a534810159b68a091726f2
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-15 23:54:49 +0200

    Init journal blueprint

---
+++ viedefamille/journal.py
@@ -0,0 +1,9 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from viedefamille.auth import login_required
+from viedefamille.db import get_db
+
+bp = Blueprint('journal', __name__)
