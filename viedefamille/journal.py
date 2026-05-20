5448d214486fca0a313e5423c157959235cad8f4
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-20 21:02:34 +0200

    apply ruff formatting

--- viedefamille/journal.py
+++ viedefamille/journal.py
@@ -1,9 +1,7 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
 from viedefamille.auth import login_required
 from viedefamille.db import get_db
 
-bp = Blueprint('journal', __name__)
+bp = Blueprint("journal", __name__)
