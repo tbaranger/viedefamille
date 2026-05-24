b59196d3aeb95627f6b311a78f723a222086ab74
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-24 20:30:59 +0200

    Initialize journal blueprint (#4)
    * Init journal blueprint
    
    * apply ruff formatting
    
    * simplify log entries schema
    
    * update log entry schema
    
    * init journal blueprint
    
    * init journal templates
    
    * implement entry addition, modification, removal

--- viedefamille/__init__.py
+++ viedefamille/__init__.py
@@ -29,12 +29,9 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth.bp)
 
-    @app.route("/")
-    def index():
-        return redirect("/journal")
+    from . import journal
 
-    @app.route("/journal")
-    def journal():
-        return render_template("journal.html")
+    app.register_blueprint(journal.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- viedefamille/schema.sql
+++ viedefamille/schema.sql
@@ -2,8 +2,6 @@ DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS family_member;
 DROP TABLE IF EXISTS family_member_user;
 DROP TABLE IF EXISTS log_entry;
-DROP TABLE IF EXISTS entry_types;
-DROP TABLE IF EXISTS entry_durations;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -29,23 +27,9 @@ CREATE TABLE log_entry (
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   family_member_id INTEGER,
-  entry_type INTEGER NOT NULL,
-  body TEXT NOT NULL,
+  entry_type TEXT NOT NULL,
+  amount INTEGER,
+  comments TEXT,
   FOREIGN KEY (author_id) REFERENCES user (id),
-  FOREIGN KEY (family_member_id) REFERENCES family_member (id),
-  FOREIGN KEY (entry_type) REFERENCES entry_types (id)
-);
-
-CREATE TABLE entry_types (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT UNIQUE NOT NULL,
-  has_duration BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE entry_durations (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  entry_id INTEGER NOT NULL,
-  start TIMESTAMP NOT NULL,
-  end TIMESTAMP,
-  FOREIGN KEY (entry_id) REFERENCES log_entry (id)
+  FOREIGN KEY (family_member_id) REFERENCES family_member (id)
 );
