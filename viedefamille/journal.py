86065ff0e4ebeca6c8b6b3415f219a2b086e5c9a
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-25 22:21:51 +0200

    implement entry types and units

--- viedefamille/journal.py
+++ viedefamille/journal.py
@@ -10,12 +10,19 @@ bp = Blueprint("journal", __name__)
 @bp.route("/")
 def index():
     db = get_db()
+
+    # Get entries of the day
     log_entries = db.execute(
         "SELECT *"
         " FROM log_entry e JOIN user u ON e.author_id = u.id"
-        " ORDER BY created DESC"
+        " WHERE DATE(e.created) = DATE('now')"
+        " ORDER BY created ASC"
     ).fetchall()
-    return render_template("journal/index.html", log_entries=log_entries)
+
+    # Get entry types
+    entry_types = db.execute("SELECT * FROM entry_type").fetchall()
+
+    return render_template("journal/index.html", log_entries=log_entries, entry_types=entry_types)
 
 
 @bp.route("/create", methods=("GET", "POST"))
@@ -23,13 +30,13 @@ def index():
 def create():
     if request.method == "POST":
         author_id = g.user["id"]
-        entry_type = request.form["entry_type"]
+        entry_type_id = request.form["entry_type_id"]
         family_member_id = request.form["family_member_id"]
         amount = request.form["amount"]
         comments = request.form["comments"]
         error = None
 
-        if not entry_type:
+        if not entry_type_id:
             error = "Entry type is required."
 
         if error is not None:
@@ -40,12 +47,12 @@ def create():
             db = get_db()
             db.execute(
                 "INSERT INTO log_entry "
-                "(author_id, entry_type, created, "
+                "(author_id, entry_type_id, created, "
                 "family_member_id, amount, comments)"
                 "VALUES (?, ?, ?, ?, ?, ?)",
                 (
                     author_id,
-                    entry_type,
+                    entry_type_id,
                     created,
                     family_member_id,
                     amount,
@@ -55,14 +62,18 @@ def create():
             db.commit()
             return redirect(url_for("journal.index"))
 
-    return render_template("journal/create.html")
+    # Get entry types for the creation form
+    db = get_db()
+    entry_types = db.execute("SELECT * FROM entry_type").fetchall()
+
+    return render_template("journal/create.html", entry_types=entry_types)
 
 
 def get_log_entry(id, check_author=True):
     log_entry = (
         get_db()
         .execute(
-            "SELECT e.id, u.username, e.entry_type, e.family_member_id, e.amount, e.comments, e.author_id"
+            "SELECT e.id, u.username, e.entry_type_id, e.family_member_id, e.amount, e.comments, e.author_id"
             " FROM log_entry e JOIN user u ON e.author_id = u.id"
             " WHERE e.id = ?",
             (id,),
@@ -85,13 +96,13 @@ def update(id):
     log_entry = get_log_entry(id)
 
     if request.method == "POST":
-        entry_type = request.form["entry_type"]
+        entry_type_id = request.form["entry_type_id"]
         family_member_id = request.form["family_member_id"]
         amount = request.form["amount"]
         comments = request.form["comments"]
         error = None
 
-        if not entry_type:
+        if not entry_type_id:
             error = "Entry type is required."
 
         if error is not None:
@@ -99,10 +110,10 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE log_entry SET entry_type = ?, family_member_id = ?, amount = ?, comments = ?"
+                "UPDATE log_entry SET entry_type_id = ?, family_member_id = ?, amount = ?, comments = ?"
                 " WHERE id = ?",
                 (
-                    entry_type,
+                    entry_type_id,
                     family_member_id,
                     amount,
                     comments,
@@ -112,7 +123,9 @@ def update(id):
             db.commit()
             return redirect(url_for("journal.index"))
 
-    return render_template("journal/update.html", log_entry=log_entry)
+    db = get_db()
+    entry_types = db.execute("SELECT * FROM entry_type").fetchall()
+    return render_template("journal/update.html", log_entry=log_entry, entry_types=entry_types)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
--- viedefamille/schema.sql
+++ viedefamille/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS family_member;
-DROP TABLE IF EXISTS family_member_user;
+DROP TABLE IF EXISTS user_family_members;
+DROP TABLE IF EXISTS entry_type;
 DROP TABLE IF EXISTS log_entry;
 
 CREATE TABLE user (
@@ -14,12 +15,18 @@ CREATE TABLE family_member (
   name TEXT NOT NULL
 );
 
-CREATE TABLE family_member_user (
-  family_member_id INTEGER NOT NULL,
+CREATE TABLE user_family_members (
   user_id INTEGER NOT NULL,
-  PRIMARY KEY (family_member_id, user_id),
-  FOREIGN KEY (family_member_id) REFERENCES family_member (id),
-  FOREIGN KEY (user_id) REFERENCES user (id)
+  family_member_id INTEGER NOT NULL,
+  PRIMARY KEY (user_id, family_member_id),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (family_member_id) REFERENCES family_member (id)
+);
+
+CREATE TABLE entry_type(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT UNIQUE NOT NULL,
+  unit TEXT
 );
 
 CREATE TABLE log_entry (
@@ -27,9 +34,10 @@ CREATE TABLE log_entry (
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   family_member_id INTEGER,
-  entry_type TEXT NOT NULL,
+  entry_type_id INTEGER NOT NULL,
   amount INTEGER,
   comments TEXT,
   FOREIGN KEY (author_id) REFERENCES user (id),
-  FOREIGN KEY (family_member_id) REFERENCES family_member (id)
+  FOREIGN KEY (family_member_id) REFERENCES family_member (id),
+  FOREIGN KEY (entry_type_id) REFERENCES entry_type (id)
 );
