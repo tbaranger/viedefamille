41c6fbf72483ce23a5f1e79e2fd5480337341130
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-26 22:33:34 +0200

    ruff and rename post to entry

--- tests/test_journal.py
+++ tests/test_journal.py
@@ -28,10 +28,10 @@ def test_login_required(client, path):
 
 
 def test_author_required(app, client, auth):
-    # change the post author to another user
+    # change the entry author to another user
     with app.app_context():
         db = get_db()
-        db.execute("UPDATE post SET author_id = 2 WHERE id = 1")
+        db.execute("UPDATE log_entry SET author_id = 2 WHERE id = 1")
         db.commit()
 
     auth.login()
@@ -52,3 +52,49 @@ def test_author_required(app, client, auth):
 def test_exists_required(client, auth, path):
     auth.login()
     assert client.post(path).status_code == 404
+
+
+def test_create(client, auth, app):
+    auth.login()
+    assert client.get("/create").status_code == 200
+    client.post("/create", data={"title": "created", "body": ""})
+
+    with app.app_context():
+        db = get_db()
+        count = db.execute("SELECT COUNT(id) FROM log_entry").fetchone()[0]
+        assert count == 2
+
+
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get("/1/update").status_code == 200
+    client.post("/1/update", data={"title": "updated", "body": ""})
+
+    with app.app_context():
+        db = get_db()
+        entry = db.execute("SELECT * FROM log_entry WHERE id = 1").fetchone()
+        assert entry["title"] == "updated"
+
+
+@pytest.mark.parametrize(
+    "path",
+    (
+        "/create",
+        "/1/update",
+    ),
+)
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={"entry_type": "", "body": ""})
+    assert b"Title is required." in response.data
+
+
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post("/1/delete")
+    assert response.headers["Location"] == "/"
+
+    with app.app_context():
+        db = get_db()
+        entry = db.execute("SELECT * FROM log_entry WHERE id = 1").fetchone()
+        assert entry is None
