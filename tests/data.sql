b4cdfd01f2ae85fe1af511fe0e5acb9235863a64
tbaranger <283795098+tbaranger@users.noreply.github.com>
2026-05-26 22:36:47 +0200

    fix test sql script

--- tests/data.sql
+++ tests/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (username, password)
+INSERT INTO user (username, password_hash)
 VALUES
   ('admin', 'scrypt:32768:8:1$9F5i8ciYcx1AFVLe$564e6357db07ae14685a74f824d74e91b46a7d47568f97c11b1b0ee395d4a7eb3b2d235b1a566393ae5f4482bdb2eb476916aa33a230fcccc600faf474f4f7e8'),
   ('other', 'scrypt:32768:8:1$EL2bGG8pCVsp32p8$4c2b2b7150b0bd0551cc12c7ea941621abdd26fd2a5c67c5f867e1521d8e0a1b423209ed37d7c00894ae5b575937424402b2177abb493bb8d866e1844be4137e');
